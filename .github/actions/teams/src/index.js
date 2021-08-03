4ebd3c9197d4eb7742f9a1864ceb43db0e8e8026
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-08-03 13:43:43 -0700

    updates

--- .github/actions/teams/src/index.js
+++ .github/actions/teams/src/index.js
@@ -5,37 +5,69 @@ const
 	{ readFileSync } = require( 'fs' ),
 	yaml = require( 'js-yaml' );
 
-let repos = [];
 
 // Setup global vars.
+let repos = [];
 const 
 	token = core.getInput( 'repo-token' ),
 	octokit = github.getOctokit( token ),
 	{ context } = github,
 	{ login: org } = context.payload.organization;
 
-
-const updateTeams = async ( teams ) => {
+/**
+ * Updates a list of teams
+ *
+ * @since 2021-08-03
+ *
+ * @param {Object}  teams          Team object.
+ * @param {?Number} parent_team_id ID of the parent team or null.
+ * @return {void}
+ */
+const updateTeams = async ( teams, parent_team_id = null ) => {
 
 	for ( let slug in teams ) {
-		await updateTeam( slug, teams[ slug ] );
+		await updateTeam( slug, teams[ slug ], parent_team_id );
 	}
 
 };
 
-const updateTeam = async ( team_slug, { name, description, permission, members = [], teams = {} } ) => {
+/**
+ * Update a single team
+ *
+ * + Updates team information
+ * + Updates team members
+ * + Adds all organization repos to the team with the specified permissions
+ * + Updates all 
+ * 
+ * @since 2021-08-03
+ *
+ * @param {string}   team_slug Team slug.
+ * @param {Object}   team             A team object.
+ * @param {string}   team.name        Team name
+ * @param {[type]}   team.description Team description.
+ * @param {[type]}   team.permission  Team repo permission.
+ * @param {string[]} team.members     Array of GitHub usernames for all team members.
+ * @param {Object}   team.teams       Child teams object.
+ * @param {?Number}  parent_team_id   ID of the parent team or null.
+ * @return {void}
+ */
+const updateTeam = async ( team_slug, { name, description, permission, members = [], teams = {} }, parent_team_id = null ) => {
+
+	console.log( `Updating team: ${ team_slug}` );
 
 	// Update team info.
-	await octokit.request( 'PATCH /orgs/{org}/teams/{team_slug}', {
+	const teamRes = await octokit.request( 'PATCH /orgs/{org}/teams/{team_slug}', {
 		org,
 		team_slug,
 		name,
 		description,
+		parent_team_id,
 	} );
 
 	// Add members.
 	for ( let i = 0; i < members.length; i++ ) {
 		
+		console.log( `Adding member: ${ members[ i ] }` );
 		await octokit.request( 'PUT /orgs/{org}/teams/{team_slug}/memberships/{username}', {
 			org,
 			team_slug,
@@ -47,6 +79,7 @@ const updateTeam = async ( team_slug, { name, description, permission, members =
 
 	// Add Repos.
 	for ( let i = 0; i < repos.length; i++ ) {
+		console.log( `Adding repo: ${ repos[ i ].name }` );
 		await octokit.request( 'PUT /orgs/{org}/teams/{team_slug}/repos/{owner}/{repo}', {
 			org,
 			team_slug,
@@ -57,7 +90,7 @@ const updateTeam = async ( team_slug, { name, description, permission, members =
 	}
 
 	// Update child teams.
-	await updateTeams( teams );
+	await updateTeams( teams, teamRes.id );
 
 
 };
@@ -74,16 +107,6 @@ const main = async () => {
 	const teamsConfig = yaml.load( readFileSync( `${ process.env.GITHUB_WORKSPACE }/teams.yml`, 'utf8' ) );
 
 	await updateTeams( teamsConfig );
-
-	// console.log( repos.map( ( { name } ) => name ) );
-
-
-
-	// const teams = await octokit.request('GET /orgs/{org}/teams', {
-	// 	org
-	// } );
-
-	// console.log( teams );
 
 }
 
